5853c702915e67ab5dbe387260495b1955312fe6
Mudassar <demoncious@gmail.com>
2020-10-31 09:42:02 -0700

    POGGERS.

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php 
-/* - Define various constants used by the application
--- BASE_PATH    = Path to the Project's root directory
--- PUBLIC_PATH  = Path to the public/static directory containing index.php
--- APP_PATH     = Path to the Source Directory of the application
--- STORE_PATH   = Path to the directory used for storing non-public files.
--- CONTENT_PATH = Path to the directory containing public files by the CMS. Should be under a PUBLIC_PATH
-*/
 
+/* Important constants to be used by the application */
 define('BASE_PATH',    __DIR__ . '/');
 define('PUBLIC_PATH',  __DIR__ . '/');
 define('APP_PATH',     BASE_PATH . 'App/');
 define('CFG_PATH',     APP_PATH . 'Config/');
-define('STORAGE_PATH',   BASE_PATH . 'storage/');
+define('STORAGE_PATH', BASE_PATH . 'storage/');
+define('BACKEND_PATH', STORAGE_PATH . 'backend/');
 define('CONTENT_PATH', STORAGE_PATH . 'app/');
 
 require_once(BASE_PATH . 'vendor/autoload.php');
